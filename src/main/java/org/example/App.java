8ac4d71080109984ac07a19bb4c6ac466c9b715a
EV-02 <ayeekee@knights.ucf.edu>
2021-09-07 23:28:41 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Alyssa Yee-Kee
+ */
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Scanner se = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = se.nextLine();
+
+        System.out.print("Who said it? ");
+        String author = se.nextLine();
+
+        System.out.println(author + " says, \"" + quote + "\"");
+    }
+}
+
